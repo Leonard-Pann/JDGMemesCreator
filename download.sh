5787feba1e9c79a5d112c943b3f1dd3a34a08d0a
Magyar57 <alxbrp@gmail.com>
2025-12-07 22:28:35 +0100

    download.sh update

--- download.sh
+++ download.sh
@@ -2,5 +2,32 @@
 
 set -e
 
-mkdir -p videos && cd videos
-yt-dlp https://www.youtube.com/@joueurdugrenier
+mkdir -p videos videos/raw
+
+# Download the raw videos
+cd videos/raw
+yt-dlp --format mp4 https://www.youtube.com/@joueurdugrenier
+
+# Remove the suffix that yt-dlp adds for no reason
+for video in *.{mp4,mkv,webm}; do
+	newname=${video// \[*\]./.}
+	if [ ! -f "$newname" ]; then
+		mv "$video" "$newname"
+	fi
+done
+
+cd ..
+
+# Convert whatever shitty format we got to mp4
+cp raw/*.mp4 .
+for video in raw/*.{mkv,webm}; do
+	filename=$(basename "$video")
+	converted=${filename/".mkv"/".mp4"}
+	converted=${filename/".webm"/".mp4"}
+	if [ ! -f "$converted" ]; then
+		echo "CONVERTING: $converted"
+		ffmpeg -loglevel warning -y -i "$video" "$converted"
+	else
+		echo "SKIPPING:   $converted"
+	fi
+done
